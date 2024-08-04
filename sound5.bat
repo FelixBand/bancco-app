9e1cc0fbbaf500d1af779ab22d966ceb47a92416
Racco-7 <samuel.donau@gmail.com>
2024-08-04 02:37:51 +0200

    geluidje fix

---
+++ sound5.bat
@@ -0,0 +1,9 @@
+@echo off
+:loop
+setlocal enabledelayedexpansion
+set /a "random_number=!random! %% 6"
+set "sounds[0]=sounds/soundtts1.wav"
+set "sounds[1]=sounds/soundfoot1.wav"
+set "sound=!sounds[%random_number%]!"
+powershell -c "(New-Object Media.SoundPlayer '%sound%').PlaySync();"
+goto loop
