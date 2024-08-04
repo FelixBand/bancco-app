c901ebde680c8f7c0c4336fc520326476c356769
Racco-7 <samuel.donau@gmail.com>
2024-08-04 02:42:42 +0200

    sounds5.bat test

--- sound5.bat
+++ sound5.bat
@@ -4,6 +4,10 @@ setlocal enabledelayedexpansion
 set /a "random_number=!random! %% 6"
 set "sounds[0]=sounds/soundtts1.wav"
 set "sounds[1]=sounds/soundfoot1.wav"
+set "sounds[2]=sounds/soundtts1.wav"
+set "sounds[3]=sounds/soundfoot1.wav"
+set "sounds[4]=sounds/soundtts1.wav"
+set "sounds[5]=sounds/soundfoot1.wav"
 set "sound=!sounds[%random_number%]!"
 powershell -c "(New-Object Media.SoundPlayer '%sound%').PlaySync();"
 goto loop
